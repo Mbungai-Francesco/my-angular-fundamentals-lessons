61e0a0fd3f71d8a8ec640ae0adda49c0c6e8a50b
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-05-09 02:32:47 +0100

    template form

--- projects/09-template-driven-forms/src/app/app.component.ts
+++ projects/09-template-driven-forms/src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [FormsModule],
   template: `
     <article>
       <h1>Blog Post</h1>
       <section>
-        <label for="title">Post Title</label>
-        <!-- add the input -->
+        <label for="inputTitle">Post Title</label>
+        <input type="text" name="" id="inputTitle" [(ngModel)] = "inputTitle">
 
         <label for="body">Post Body</label>
-        <!-- add the textarea -->
+        <textarea name="" id="body" [(ngModel)] = "body" ></textarea>
+
       </section>
-      <!-- <section>
-        <p>Display title</p>
-        <p>Display value</p>
-      </section> -->
+      <section>
+        <p>{{inputTitle}}</p>
+        <p>{{body}}</p>
+      </section>
     </article>
   `,
 })
 export class AppComponent {
   title = '09-template-driven-forms';
+  inputTitle : string = '';
+  body : string = '';
 }
